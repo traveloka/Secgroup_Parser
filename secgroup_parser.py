2f2abbc308ed2c89ea6480d73ab9429799f0b157
bernard <bernard@traveloka.com>
2017-06-06 14:27:22 +0700

    add cidr_ips and group ids

--- secgroup_parser.py
+++ secgroup_parser.py
@@ -1,6 +1,5 @@
 import csv
 import re
-common_rules = ['office_ip_vpn']
 rules_in_shared_secgroup = ['data-mongod','dim-mongos','dwh-mongod','psm-kafka','release-dbscript','troubleshoot-dbscript']
 
 def parse_rules_from_csv(csv_file):
@@ -8,7 +7,7 @@ def parse_rules_from_csv(csv_file):
     with open(csv_file, 'r') as csv_sg:
         sg_rules = csv.reader(csv_sg, delimiter=',')
         for rule in sg_rules:
-            if not rule[0] in rules_in_shared_secgroup or not rule[1] in rules_in_shared_secgroup:
+            if not rule[0] in rules_in_shared_secgroup and not rule[1] in rules_in_shared_secgroup:
                rules.append(rule)
     return rules
 
@@ -18,18 +17,26 @@ def populate_groups(rules):
     for rule in rules:
         if (
             (rule[0] not in groups) and
-            (rule[0] not in common_rules) and
             (is_cidr(rule[0]) is False)
         ):
             groups.append(rule[0])
         if (
             (rule[1] not in groups) and
-            (rule[1] not in common_rules) and
             (is_cidr(rule[0]) is False)
         ):
             groups.append(rule[1])
     return groups
 
+def is_sg_id(sg):
+    pattern = re.compile(r"sg-[a-z0-9]{8}")
+    try:
+        match_sg_id = re.match(pattern,sg)
+        if len(match_sg_id.group()) > 0:
+           return True
+        else:
+           return False
+    except:
+        return False
 
 def is_cidr(ip):
     pattern = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:/\d{1,2})?")
@@ -60,6 +67,7 @@ def print_rules(direction, rules):
             ports.append([rule[2], rule[3], rule[4]])
     for port in ports:
         count_ips = []
+        count_sg_ids = []
         groups = []
         for rule in rules:
             if [rule[2], rule[3], rule[4]] == port:
@@ -67,19 +75,46 @@ def print_rules(direction, rules):
                 if (direction == "in"):
                     if(is_cidr(rule[0])):
                         count_ips.append(rule)
+                    if(is_sg_id(rule[0])):
+                        count_sg_ids.append(rule)
                 if (direction == "out"):
                     if(is_cidr(rule[1])):
                         count_ips.append(rule)
+                    if(is_sg_id(rule[1])):
+                        count_sg_ids.append(rule)
         groups = [x for x in groups if (x not in count_ips)]
-        print("\t- group_names:").expandtabs(2)
-        for rule in groups:
-            if direction == "in":
-                col = rule[0]
-            elif direction == "out":
-                col = rule[1]
-            print("\t\t\t- %s" % (col)).expandtabs(2)
+        groups = [x for x in groups if (x not in count_sg_ids)]
+        new_rules=True
+        if len(groups) > 0:
+            new_rules=False
+            print("\t- group_names:").expandtabs(2)
+            for rule in groups:
+                if direction == "in":
+                    col = rule[0]
+                elif direction == "out":
+                    col = rule[1]
+                print("\t\t\t- %s" % (col)).expandtabs(2)
+        
+        
+        if len(count_sg_ids) > 0:
+           if new_rules:
+              new_rules = False
+              print("\t- group_ids:").expandtabs(2)
+           else:
+              print("\t\tgroup_ids:").expandtabs(2)
+           for rule in count_sg_ids:
+               if direction == "in":
+                  col = rule[0]
+               elif direction == "out":
+                  col = rule[1]
+               print("\t\t\t- %s" % (col)).expandtabs(2)
+
         if(len(count_ips) > 0):
-            print("\t\tcidr_ips:").expandtabs(2)
+            if new_rules:
+               new_rules = False
+               print("\t- cidr_ips:").expandtabs(2)
+            else:
+               print("\t\tcidr_ips").expandtabs(2)
             for rule in count_ips:
                 if direction == "in":
                     col = rule[0]
@@ -87,11 +122,19 @@ def print_rules(direction, rules):
                     col = rule[1]
                 print("\t\t\t- '%s'" % (col)).expandtabs(2)
         print("\t\trules:").expandtabs(2)
-        print("\t\t\t- from: %s" % port[0]).expandtabs(2)
-        print("\t\t\t\tto: %s" % port[1]).expandtabs(2)
-        print("\t\t\t\tproto: %s" % port[2]).expandtabs(2)
+        print("\t\t\t- from_port: %s" % port[0]).expandtabs(2)
+        print("\t\t\t\tto_port: %s" % port[1]).expandtabs(2)
+        print("\t\t\t\tproto: \"%s\"" % port[2]).expandtabs(2)
+    print ""
 
-
+def print_related_secgroup(group,rules):
+    print("related secgroup:")
+    for rule in rules:
+        if group == rule[0] and not is_cidr(rule[1]):
+           print("\t- %s" % rule[1]).expandtabs(2)
+        elif group == rule[1] and not is_cidr(rule[0]):
+           print("\t- %s" % rule[0]).expandtabs(2)
+    
 def execution_checklists(groups):
     for group in groups:
         print(
@@ -110,14 +153,15 @@ if __name__ == '__main__':
 
     print("\n* List of Connectivity Changes")
     for group in groups:
-        print("\n%s" % (group)).expandtabs(2)
-        print("===")
+        print("# %s #" % (group)).expandtabs(2)
+        print_related_secgroup(group,rules)
+        print ""
         egress_rules = parse_rules("out", group, rules)
         ingress_rules = parse_rules("in", group, rules)
         if (len(ingress_rules) > 0):
             print("secgroup_ingress_acls:").expandtabs(2)
             print_rules("in", ingress_rules)
-
+        
         if (len(egress_rules) > 0):
             print("secgroup_egress_acls:").expandtabs(2)
             print_rules("out", egress_rules)
